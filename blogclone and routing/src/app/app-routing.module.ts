cdcecfb8cd4d956a31a3ec77e5e4d3302eebe622
Ash2499 <87962620+Ash2499@users.noreply.github.com>
2021-10-03 13:17:32 +0530

    Add files via upload
    failed to apply routing, navigate between components

---
+++ blogclone and routing/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DocComponent } from './doc/doc.component';
+import { BlogComponent } from './blog/blog.component';
+
+const routes: Routes = [
+  { path: "Home", component: HomeComponent},
+  { path: "doc", component: DocComponent},
+  { path: "blog", component: BlogComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [HomeComponent,DocComponent,BlogComponent]
+
